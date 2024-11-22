ec18ba12bd868ae930e9ef67d9c83a4ee2ec895e
ast-tnbt <trinhtin272@gmail.com>
2024-11-22 23:45:50 +0700

    fix login admin logic and dashboard UI

--- Admin/sidenav.php
+++ Admin/sidenav.php
@@ -1,17 +1,15 @@
 <?php
-
+if (session_status() == PHP_SESSION_NONE) {
+session_start();
+}
+if (empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+  }
 if (!isset($_SESSION['admin_name'])) {
-    $_SESSION['msg'] = "You must log in first";
-    header('Location: login.php');
+    header('Location: ./login.php');
     exit;
 }
 
-if (isset($_GET['logout'])) {
-    session_destroy();
-    unset($_SESSION['admin_name']);
-    header('Location: login.php');
-    exit;
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -42,7 +40,7 @@ if (isset($_GET['logout'])) {
 </head>
 
 <body class="dark-edition">
-    <div class="wrapper">
+    <div class="wrapper position-fixed">
         <!-- Sidebar -->
         <div class="sidebar" data-color="purple" data-background-color="black" data-image="../assets/img/sidebar-2.jpg">
             <div class="logo">
