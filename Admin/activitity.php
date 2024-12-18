9641c00a665cf17b7991c1bb981b2f0eabba6c4f
ast-tnbt <trinhtin272@gmail.com>
2024-12-18 17:50:54 +0700

    fix UI dashboard admin

--- Admin/activitity.php
+++ Admin/activitity.php
@@ -16,75 +16,79 @@ function countRows($con, $table) {
 }
 ?>
 
-<div class="row" style="padding-top: 10vh;">
-    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-        <div class="card card-stats">
-            <div class="card-header card-header-warning card-header-icon">
-                <div class="card-icon">
-                    <i class="material-icons">content_copy</i>
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-header card-header-warning card-header-icon">
+                        <div class="card-icon">
+                            <i class="fa-solid fa-user"></i>
+                        </div>
+                        <p class="card-category">Tổng thành viên</p>
+                        <h3 class="card-title">
+                            <?php 
+                            $con=OpenCon();
+                            echo countRows($con, 'user_info');
+                            CloseCon($con);
+                            ?>
+                        </h3>
+                    </div>
                 </div>
-                <p class="card-category">Tổng thành viên</p>
-                <h3 class="card-title">
-                    <?php 
-                    $con=OpenCon();
-                    echo countRows($con, 'user_info');
-                    CloseCon($con);
-                    ?>
-                </h3>
             </div>
-        </div>
-    </div>
-
-    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-        <div class="card card-stats">
-            <div class="card-header card-header-success card-header-icon">
-                <div class="card-icon">
-                    <i class="material-icons">store</i>
+        
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-header card-header-success card-header-icon">
+                        <div class="card-icon">
+                            <i class="fa-solid fa-table"></i>
+                        </div>
+                        <p class="card-category">Tổng phân loại</p>
+                        <h3 class="card-title">
+                            <?php 
+                          $con=OpenCon();
+                            echo countRows($con, 'categories'); 
+                            CloseCon($con);
+                            ?>
+                        </h3>
+                    </div>
                 </div>
-                <p class="card-category">Tổng phân loại</p>
-                <h3 class="card-title">
-                    <?php 
-                  $con=OpenCon();
-                    echo countRows($con, 'categories'); 
-                    CloseCon($con);
-                    ?>
-                </h3>
             </div>
-        </div>
-    </div>
-
-    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-        <div class="card card-stats">
-            <div class="card-header card-header-danger card-header-icon">
-                <div class="card-icon">
-                    <i class="material-icons">info_outline</i>
+        
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-header card-header-danger card-header-icon">
+                        <div class="card-icon">
+                            <i class="fa-solid fa-shop"></i>
+                        </div>
+                        <p class="card-category">Tổng khách mua</p>
+                        <h3 class="card-title">
+                            <?php 
+                            $con=OpenCon();
+                            echo countRows($con, 'user_info');
+                            CloseCon($con);
+                            ?>
+                        </h3>
+                    </div>
                 </div>
-                <p class="card-category">Tổng khách mua</p>
-                <h3 class="card-title">
-                    <?php 
-                    $con=OpenCon();
-                    echo countRows($con, 'user_info');
-                    CloseCon($con);
-                    ?>
-                </h3>
             </div>
-        </div>
-    </div>
-
-    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-        <div class="card card-stats">
-            <div class="card-header card-header-info card-header-icon">
-                <div class="card-icon">
-                    <i class="fa fa-twitter"></i>
+        
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-header card-header-info card-header-icon">
+                        <div class="card-icon">
+                            <i class="fa-solid fa-money-bills"></i>
+                        </div>
+                        <p class="card-category">Tổng hóa đơn</p>
+                        <h3 class="card-title">
+                            <?php 
+                            $con=OpenCon();
+                            echo countRows($con, 'orders_info'); 
+                            CloseCon($con);
+                            ?>
+                        </h3>
+                    </div>
                 </div>
-                <p class="card-category">Tổng hóa đơn</p>
-                <h3 class="card-title">
-                    <?php 
-                    $con=OpenCon();
-                    echo countRows($con, 'orders_info'); 
-                    CloseCon($con);
-                    ?>
-                </h3>
             </div>
         </div>
     </div>
