99a768081adfd7f71d9a0ad34c4affc7e4928bbb
nguyenchivi001 <nguyenchivi292004@gmail.com>
2024-12-19 13:23:29 +0700

    Create order successful page

---
+++ Default/order_successful.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/order_successful.css">
+  </head>
+    <body>
+      <div class="card">
+      <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
+        <i class="checkmark">✓</i>
+      </div>
+        <h1>Đặt hàng thành công</h1> 
+        <p>Chúng tôi đã nhận được yêu cầu mua hàng của bạn;<br/> Chúng tôi sẽ liên lạc sớm nhất!</p>
+        <div class="heading">
+            <br><br><br>
+                <a href="index.php" style="float:left" class="continue">Tiếp tục mua sắm</a>
+                <a href="my_orders.php" style="float:right" class="continue">Đơn hàng của tôi</a>
+            </div>
+      </div>
+
+    </body>
+</html>
