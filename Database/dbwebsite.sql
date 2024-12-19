e3acfb701599f6b03148e3a68963a6b6530728d7
nguyenchivi001 <nguyenchivi292004@gmail.com>
2024-12-19 11:29:24 +0700

    Edit database

--- Database/dbwebsite.sql
+++ Database/dbwebsite.sql
@@ -13,15 +13,16 @@ CREATE TABLE `admin_info` (
   `admin_id` int(10) NOT NULL,
   `admin_name` varchar(100) NOT NULL,
   `admin_email` varchar(300) NOT NULL,
-  `admin_password` varchar(300) NOT NULL
+  `admin_password` varchar(300) NOT NULL,
+  `role` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `admin_info`
 --
 
-INSERT INTO `admin_info` (`admin_id`, `admin_name`, `admin_email`, `admin_password`) VALUES
-(1, 'admin', 'admin@gmail.com', '25f9e794323b453885f5181f1b624d0b');
+INSERT INTO `admin_info` (`admin_id`, `admin_name`, `admin_email`, `admin_password`, `role`) VALUES
+(1, 'admin', 'admin@gmail.com', '25f9e794323b453885f5181f1b624d0b','Manager');
 
 -- --------------------------------------------------------
 
